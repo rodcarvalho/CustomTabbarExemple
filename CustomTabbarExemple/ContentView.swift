4f3eb7d858616ce464e80711c6f96c05d97dcb7e
rodcarvalho <barretocarvalho.contato@gmail.com>
2021-08-23 14:19:19 -0300

    Initial Commit

---
+++ CustomTabbarExemple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTabbarExemple
+//
+//  Created by Rodrigo Carvalho on 23/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomTabbarExemple/CustomTabbarExempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabbarExempleApp.swift
+//  CustomTabbarExemple
+//
+//  Created by Rodrigo Carvalho on 23/08/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabbarExempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
